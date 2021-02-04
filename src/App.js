e662e8593458b478223a8c789ec7f9282d507b4a
Khushboo Goyal <61690410+khushboogoyal88@users.noreply.github.com>
2021-02-05 00:18:59 +0200

    App.js updated to new rest API

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const [letters, setLetters] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [query, setQuery] = useState("");
-
+  
   const [letter, setLetter] = useState({
     name: "",
     age: "",
@@ -20,82 +20,83 @@ const App = () => {
     present: "",
     message: "",
   });
-
+  
   useEffect(() => {
     const fetchItems = () => {
-      axios
-        .get(`http://localhost:5000/letters`)
-        .then((resp) => {
-          const data = resp.data;
-          setLetters(data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
+      axios.get(`api/v1/letters/`)
+      .then((res) => {
+        const data = res.data.data;
+        console.log(data);
+        setLetters(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+      
       setIsLoading(false);
     };
-
+    
     fetchItems();
   }, []);
-
+  
   const changeHandler = (e) => {
     setLetter({
       ...letter,
       [e.target.name]: e.target.value,
     });
   };
-
+  
   const submitHandler = () => {
     if (window.confirm("Should we send your letter to Santa?")) {
-      axios.post("http://localhost:5000/letters", letter);
+      axios.post("api/v1/letters/", letter);
       window.location.reload();
     }
   };
-
+  
   const getQuery = (q) => {
     setQuery(q);
   };
-
-  const removeLetter = (id) => {
-    axios.delete(`http://localhost:5000/letters/${id}`).then((resp) => {
+  
+  const removeLetter = (_id) => {
+    axios.delete(`api/v1/letters/${_id}`).then((resp) => {
       const data = resp.data;
       setLetters(data);
     });
     window.location.reload();
   };
-
+  
   return (
     <Router>
-      <Nav title="Letter2Santa" icon="email" />
-      <div className="container wrapper ">
-        <Switch>
-          <Route exact path="/">
-            <Fragment>
-              <Form
-                change={changeHandler}
-                submit={submitHandler}
-                removeLetter={removeLetter}
-              />
-            </Fragment>
-          </Route>
-          <Route path="/letters">
-            <LettersGrid
-              isLoading={isLoading}
-              letters={letters}
-              removeLetter={removeLetter}
-            />
-          </Route>
-          <Route path="/:user">
-            <h3>User</h3>
-          </Route>
-          <Route>
-            <h3>No Match</h3>
-          </Route>
-        </Switch>
-      </div>
+    <Nav title="Letter2Santa" icon="email" />
+    <div className="container wrapper ">
+    <Switch>
+    <Route exact path="/">
+    <Fragment>
+    <Form
+    change={changeHandler}
+    submit={submitHandler}
+    removeLetter={removeLetter}
+    />
+    </Fragment>
+    </Route>
+    <Route path="/letters">
+    <LettersGrid
+    isLoading={isLoading}
+    letters={letters}
+    removeLetter={removeLetter}
+    />
+    </Route>
+    <Route path="/:user">
+    <h3>User</h3>
+    </Route>
+    <Route>
+    <h3>No Match</h3>
+    </Route>
+    </Switch>
+    </div>
     </Router>
-  );
-};
-
-export default App;
+    );
+  };
+  
+  export default App;
+  
